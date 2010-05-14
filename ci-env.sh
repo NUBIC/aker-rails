09b1b5882472181aa8e044b8578d00c05afad8e7
Rhett Sutphin <rhett@detailedbalance.net>
2010-05-13 20:10:53 -0500

    Bash environment setup for CI.  #2396.
    Ensures that:
      - jruby is installed and on the path for celerity
      - we are testing with ree

---
+++ ci-env.sh
@@ -0,0 +1,37 @@
+######
+# This is not an executable script.  It selects and configures rvm for
+# bcsec's CI process based on the BCSEC_ENV environment variable.
+#
+# Use it by sourcing it:
+#
+#  . ci-rvm.sh
+#
+# Assumes that the create-on-use settings are set in your ~/.rvmrc:
+#
+#  rvm_install_on_use_flag=1
+#  rvm_gemset_create_on_use_flag=1
+
+set +x
+echo ". ~/.rvm/scripts/rvm"
+. ~/.rvm/scripts/rvm
+set -x
+
+BCSEC_JRUBY='jruby-1.4.0'
+BCSEC_RVM_RUBY='ree-1.8.7-2010.01'
+
+echo "Adding jruby to the PATH for culerity"
+set +x
+rvm use "${BCSEC_JRUBY}" # ensure it is installed
+set -x
+mkdir -p ci_bin
+if [ -L ci_bin/jruby ]; then
+  rm ci_bin/jruby
+fi
+ln -s ~/.rvm/bin/${BCSEC_JRUBY} ci_bin/jruby
+PATH="ci_bin:$PATH"
+
+echo "Switching to ${BCSEC_RVM_RUBY}"
+set +x
+rvm use "$BCSEC_RVM_RUBY"
+set -x
+ruby -v
