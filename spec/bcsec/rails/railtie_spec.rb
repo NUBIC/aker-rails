7232e7191193b11325490ea128407ff0b71c91c8
David Yip <yipdw@northwestern.edu>
2011-02-05 14:59:28 -0600

    Clarified reason for pending examples in the Bcsec::Rails::Railtie spec.  #2805.

--- spec/bcsec/rails/railtie_spec.rb
+++ spec/bcsec/rails/railtie_spec.rb
@@ -2,10 +2,16 @@ require File.expand_path("../../../spec_helper", __FILE__)
 
 module Bcsec::Rails
   describe Railtie do
-    it "uses Rails' logger for Bcsec logging"
+    it "uses Rails' logger for Bcsec logging" do
+      pending "need to figure out a good way to test railties"
+    end
 
-    it "installs the Bcsec middleware"
+    it "installs the Bcsec middleware" do
+      pending "need to figure out a good way to test railties"
+    end
 
-    it "sets up a to_prepare hook for Bcsec::Rails::Application"
+    it "sets up a to_prepare hook for Bcsec::Rails::Application" do
+      pending "need to figure out a good way to test railties"
+    end
   end
 end
