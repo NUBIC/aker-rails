cd0eb5b0a84e8d7c4b233873045d564eab334dae
David Yip <yipdw@northwestern.edu>
2011-02-05 00:08:12 -0600

    Ported serenity-30 to Rails 3.0.3.  #2805.

--- test-applications/serenity-30/config/routes.rb
+++ test-applications/serenity-30/config/routes.rb
@@ -1,41 +1,6 @@
-ActionController::Routing::Routes.draw do |map|
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # See how all your routes lay out with "rake routes"
-
-  map.root :controller => 'public', :action => 'view', :format => 'text'
-  map.connect 'portal', :controller => 'public', :action => 'portal', :format => 'text'
-  map.connect 'protected',
-    :controller => 'protected', :action => 'authentication_only', :format => 'text'
-  map.connect 'owners',
-    :controller => 'permit', :action => 'owners_only', :format => 'text'
+Serenity30::Application.routes.draw do
+  match '/' => 'public#view', :format => 'text'
+  match 'portal' => 'public#portal', :format => 'text'
+  match 'protected' => 'protected#authentication_only', :format => 'text'
+  match 'owners' => 'permit#owners_only', :format => 'text'
 end
