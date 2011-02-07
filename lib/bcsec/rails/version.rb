275d375a93c2455ad187c38b2c69a352a4cf2d0c
David Yip <yipdw@northwestern.edu>
2011-02-07 13:23:55 -0600

    Bumped version to 3.0.0.pre.  #2805.

--- lib/bcsec/rails/version.rb
+++ lib/bcsec/rails/version.rb
@@ -2,6 +2,6 @@ module Bcsec
   module Rails
     # VERSION is in a separate file with no external dependencies so it
     # can be sourced from the gemspec.
-    VERSION = "2.0.1.pre"
+    VERSION = "3.0.0.pre"
   end
 end
