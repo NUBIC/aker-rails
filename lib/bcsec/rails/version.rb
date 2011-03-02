d6288fd8a4679503f22e422fc5c2265378b64364
Rhett Sutphin <r-sutphin@northwestern.edu>
2011-03-02 17:22:33 -0600

    Version 3.0.0

--- lib/bcsec/rails/version.rb
+++ lib/bcsec/rails/version.rb
@@ -2,6 +2,6 @@ module Bcsec
   module Rails
     # VERSION is in a separate file with no external dependencies so it
     # can be sourced from the gemspec.
-    VERSION = "3.0.0.pre"
+    VERSION = "3.0.0"
   end
 end
