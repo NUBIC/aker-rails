b34378580ff778cf2df2dab24a9db4a74ef14861
Rhett Sutphin <rhett@detailedbalance.net>
2011-07-20 11:35:15 -0500

    Update for continued development.

--- lib/aker/rails/version.rb
+++ lib/aker/rails/version.rb
@@ -2,6 +2,6 @@ module Aker
   module Rails
     # VERSION is in a separate file with no external dependencies so it
     # can be sourced from the gemspec.
-    VERSION = "3.0.1"
+    VERSION = "3.0.2.pre"
   end
 end
