83cbeb62e1b306fb13419d7d6a64c69ac9de7e42
Rhett Sutphin <rhett@detailedbalance.net>
2011-12-28 14:36:38 -0600

    Convert to single CI script. Closes #3.

---
+++ ci-exec.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -xe
+
+BUNDLER_VERSION=1.1.rc.6
+GEMSET=aker-rails
+
+export TMPDIR="`pwd`/tmp"
+mkdir -p tmp
+
+if [ -n "${RAILS_VERSION}" ]; then
+    GEMSET="${GEMSET}-${RAILS_VERSION}"
+fi
+
+if [ -z $CI_RUBY ]; then
+    echo "CI_RUBY must be set"
+    exit 1
+fi
+
+set +xe
+echo "Initializing RVM"
+source ~/.rvm/scripts/rvm
+set -xe
+
+# On the overnight build, reinstall all gems
+if [ `date +%H` -lt 5 ]; then
+    set +xe
+    echo "Purging gemset to verify that all deps can still be installed"
+    rvm --force $CI_RUBY gemset delete $GEMSET
+    set -xe
+fi
+
+RVM_CONFIG="${CI_RUBY}@${GEMSET}"
+set +xe
+echo "Switching to ${RVM_CONFIG}"
+rvm use $RVM_CONFIG
+set -xe
+
+which ruby
+ruby -v
+gem -v
+
+set +e
+gem list -i bundler -v $BUNDLER_VERSION
+if [ $? -ne 0 ]; then
+  set -e
+  gem install bundler -v $BUNDLER_VERSION
+fi
+set -e
+
+bundle _${BUNDLER_VERSION}_ update
+
+bundle _${BUNDLER_VERSION}_ exec rake autobuild --trace
