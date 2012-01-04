059cb133f622b378ed71820c328affb396687f84
David Yip <yipdw@northwestern.edu>
2012-01-04 16:12:13 -0600

    Version 3.0.2.

--- lib/aker/rails/version.rb
+++ lib/aker/rails/version.rb
@@ -2,6 +2,6 @@ module Aker
   module Rails
     # VERSION is in a separate file with no external dependencies so it
     # can be sourced from the gemspec.
-    VERSION = "3.0.2.pre"
+    VERSION = "3.0.2"
   end
 end
