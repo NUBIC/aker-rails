e428bda00742891d705e8b7faa6f6e18a4a00dd2
David Yip <yipdw@member.fsf.org>
2013-08-19 12:30:49 -0500

    Version 4.0.0.pre.

--- lib/aker/rails/version.rb
+++ lib/aker/rails/version.rb
@@ -2,6 +2,6 @@ module Aker
   module Rails
     # VERSION is in a separate file with no external dependencies so it
     # can be sourced from the gemspec.
-    VERSION = "3.0.3.pre"
+    VERSION = "4.0.0.pre"
   end
 end
